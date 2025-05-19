221429e4a5294d5cfe16113006f53089a3204042
Pete Gadomski <pete.gadomski@gmail.com>
2025-05-19 14:23:24 -0600

    --wip-- [skip ci]

---
+++ crates/wasm/src/lib.rs
@@ -0,0 +1,22 @@
+use arrow_array::RecordBatchIterator;
+use arrow_schema::ArrowError;
+use arrow_wasm::{Table, error::WasmResult};
+use thiserror::Error;
+use wasm_bindgen::prelude::*;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error(transparent)]
+    Arrow(#[from] ArrowError),
+}
+
+#[wasm_bindgen(js_name = arrowToStacJson)]
+pub fn arrow_to_stac_json(table: Table) -> WasmResult<JsValue> {
+    let reader = RecordBatchIterator::new(
+        table.record_batches().into_iter().map(From::from).map(Ok),
+        table.schema().into(),
+    );
+    let items = stac::geoarrow::json::from_record_batch_reader(reader)?;
+    let items = serde_wasm_bindgen::to_value(&items)?;
+    Ok(items)
+}
