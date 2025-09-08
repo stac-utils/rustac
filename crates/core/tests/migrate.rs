b1ae6c60f67410e4b048df7a9cb426d804944232
Francisco Jiménez <49205177+jjcfrancisco@users.noreply.github.com>
2025-09-08 19:10:48 +0100

    feat: implement async jsonschema validation (#798)
    ## Closes
    
    Add async JSON Schema validation:
    [#627](https://github.com/stac-utils/rustac/issues/627?utm_source=chatgpt.com)
    
    ## Description
    
    Adds asynchronous JSON Schema validation via [async
    retrieve](https://github.com/Stranger6667/jsonschema/pull/703)
    
    ## Checklist
    
    Delete any checklist items that do not apply (e.g. if your change is
    minor, it may not require documentation updates).
    
    - [x] Unit tests
    - [x] Documentation, including doctests
    - [x] Git history is linear
    - [x] Commit messages are descriptive
    - [ ] (optional) Git commit messages follow [conventional
    commits](https://www.conventionalcommits.org/en/v1.0.0/)
    - [x] Code is formatted (`cargo fmt`)
    - [x] `cargo test`
    
    <!-- markdownlint-disable-file MD041 -->

--- crates/core/tests/migrate.rs
+++ crates/core/tests/migrate.rs
@@ -4,8 +4,9 @@ use stac_validate::Validate;
 use std::path::PathBuf;
 
 #[rstest]
-fn v1_0_0_to_v1_1_0(#[files("../../spec-examples/v1.0.0/**/*.json")] path: PathBuf) {
+#[tokio::test]
+async fn v1_0_0_to_v1_1_0(#[files("../../spec-examples/v1.0.0/**/*.json")] path: PathBuf) {
     let value: Value = stac::read(path.to_str().unwrap()).unwrap();
     let value = value.migrate(&Version::v1_1_0).unwrap();
-    value.validate().unwrap();
+    value.validate().await.unwrap();
 }
