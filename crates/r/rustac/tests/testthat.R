20f74e3a349848829561f4073e03e376ce85e272
Pete Gadomski <pete.gadomski@gmail.com>
2026-02-27 11:41:11 -0500

    fix: redunant directory

--- crates/r/rustac/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(rustac)
-
-test_check("rustac")
--- crates/r/rustac/tests/testthat/setup.R
+++
@@ -1,7 +0,0 @@
-workspace_root <- normalizePath(file.path(
-  testthat::test_path(), "..", "..", "..", ".."
-))
-
-fixture <- function(...) {
-  file.path(workspace_root, ...)
-}
